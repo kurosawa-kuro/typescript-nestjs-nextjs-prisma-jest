b325072300713af7f05ac1cd62e99fee493010d9
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-10-14 01:51:44 +0900

    Refactor getHello method to use async/await and add Prisma service

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from './prisma.service';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
+  constructor(private prisma: PrismaService) {}
+
+  async getHello(): Promise<string> {
+    // sampleテーブルにデータを挿入
+    const sample = await this.prisma.sample.create({
+      data: {
+        title: 'sample',
+      },
+    });
+    return sample.title;
   }
 }
