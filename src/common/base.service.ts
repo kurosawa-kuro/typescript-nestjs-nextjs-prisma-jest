31a8c7416bce6ce53e0fb2c27903daffbfe7fc6a
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-10-14 21:12:58 +0900

    Refactor BaseService to use PrismaService and add generic types

--- src/common/base.service.ts
+++ src/common/base.service.ts
@@ -1,21 +1,63 @@
 import { NotFoundException } from '@nestjs/common';
-import { PrismaClient } from '@prisma/client';
+import { PrismaService } from '../database/prisma.service';
 
-export abstract class BaseService<T> {
-  protected abstract prisma: PrismaClient;
+export abstract class BaseService<T, CreateInput, UpdateInput, WhereUniqueInput, WhereInput> {
   protected abstract entityName: string;
 
-  abstract create(data: Partial<T>): Promise<T>;
+  constructor(protected prisma: PrismaService) {}
 
-  abstract all(): Promise<T[]>;
+  async create(data: CreateInput): Promise<T> {
+    return this.getRepository().create({ data }) as Promise<T>;
+  }
 
-  abstract findById(id: number): Promise<T>;
+  async all(): Promise<T[]> {
+    return this.getRepository().findMany() as Promise<T[]>;
+  }
 
-  abstract update(id: number, data: Partial<T>): Promise<T>;
+  async findById(id: number): Promise<T> {
+    const entity = await this.getRepository().findUnique({ where: { id } as WhereUniqueInput }) as T | null;
+    if (!entity) {
+      this.handleNotFound(id);
+    }
+    return entity as T;
+  }
 
-  abstract destroy(id: number): Promise<void>;
+  async update(id: number, data: UpdateInput): Promise<T> {
+    try {
+      return await this.getRepository().update({
+        where: { id } as WhereUniqueInput,
+        data,
+      }) as Promise<T>;
+    } catch (error) {
+      if (error.code === 'P2025') {
+        this.handleNotFound(id);
+      }
+      throw error;
+    }
+  }
+
+  async destroy(id: number): Promise<void> {
+    try {
+      await this.getRepository().delete({ where: { id } as WhereUniqueInput });
+    } catch (error) {
+      if (error.code === 'P2025') {
+        this.handleNotFound(id);
+      }
+      throw error;
+    }
+  }
+
+  async findFirst(where: WhereInput): Promise<T> {
+    const entity = await this.getRepository().findFirst({ where }) as T | null;
+    if (!entity) {
+      throw new NotFoundException(`${this.entityName} not found with criteria: ${JSON.stringify(where)}`);
+    }
+    return entity;
+  }
 
   protected handleNotFound(id: number): never {
     throw new NotFoundException(`${this.entityName} with ID ${id} not found`);
   }
+
+  protected abstract getRepository(): any;
 }
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,69 +1,23 @@
-import { Injectable, NotFoundException } from '@nestjs/common';
+import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../database/prisma.service';
 import { User, Prisma } from '@prisma/client';
 import { BaseService } from '../common/base.service';
 
 @Injectable()
-export class UserService extends BaseService<User> {
+export class UserService extends BaseService<
+  User,
+  Prisma.UserCreateInput,
+  Prisma.UserUpdateInput,
+  Prisma.UserWhereUniqueInput,
+  Prisma.UserWhereInput
+> {
   protected entityName = 'User';
 
   constructor(protected prisma: PrismaService) {
-    super();
+    super(prisma);
   }
 
-  async create(data: Partial<User>): Promise<User> {
-    return this.prisma.user.create({ data: data as Prisma.UserCreateInput });
-  }
-
-  async all(): Promise<User[]> {
-    return this.prisma.user.findMany();
-  }
-
-  async findById(id: number): Promise<User> {
-    const user = await this.prisma.user.findUnique({ where: { id } });
-    if (!user) {
-      this.handleNotFound(id);
-    }
-    return user;
-  }
-
-  async update(id: number, data: Prisma.UserUpdateInput): Promise<User> {
-    try {
-      return await this.prisma.user.update({
-        where: { id },
-        data,
-      });
-    } catch (error) {
-      if (error.code === 'P2025') {
-        this.handleNotFound(id);
-      }
-      throw error;
-    }
-  }
-
-  async destroy(id: number): Promise<void> {
-    try {
-      await this.prisma.user.delete({
-        where: { id },
-      });
-    } catch (error) {
-      throw new NotFoundException(`User with ID ${id} not found`);
-    }
-  }
-
-  async find(id: number): Promise<User> {
-    const user = await this.prisma.user.findUnique({ where: { id } });
-    if (!user) {
-      throw new NotFoundException(`User with ID ${id} not found`);
-    }
-    return user;
-  }
-
-  async findFirst(where: Prisma.UserWhereInput): Promise<User> {
-    const user = await this.prisma.user.findFirst({ where });
-    if (!user) {
-      throw new NotFoundException('User not found');
-    }
-    return user;
+  protected getRepository() {
+    return this.prisma.user;
   }
 }
