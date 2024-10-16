2ec83cdb73d7e25893ec16dbfa8f185356b2aa38
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-10-16 16:14:19 +0900

    Refactor import paths for AuthController and SignupDto

--- backend/src/user/user.module.ts
+++ backend/src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
+import { UserController } from './user.controller';
 import { PrismaModule } from '../database/prisma.module';
 
 @Module({
   imports: [PrismaModule],
+  controllers: [UserController],
   providers: [UserService],
   exports: [UserService],
 })
