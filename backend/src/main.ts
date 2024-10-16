ae266b0e6212d19186144d8272c900d3687cf657
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-10-16 17:35:22 +0900

    Refactor CORS configuration in main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -4,6 +4,12 @@ import * as cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: 'http://localhost:3000', // Next.jsアプリのURL
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  });
   app.use(cookieParser());
   await app.listen(3001);
 }
