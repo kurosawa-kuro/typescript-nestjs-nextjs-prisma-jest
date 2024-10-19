9a6110c21954020fc70d520568af90e993350032
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-10-19 13:12:02 +0900

    Refactor package.json and main.ts files

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -2,9 +2,15 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
 import * as morgan from 'morgan';
+import { AllExceptionFilter } from './filters/all-exception.filter';
+import { HttpAdapterHost } from '@nestjs/core';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  
+  const httpAdapter = app.get(HttpAdapterHost);
+  app.useGlobalFilters(new AllExceptionFilter(httpAdapter));
+
   app.enableCors({
     origin: 'http://localhost:3000', // Next.jsアプリのURL
     methods: ['GET', 'POST', 'PUT', 'DELETE'],
