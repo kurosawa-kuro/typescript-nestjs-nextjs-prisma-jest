8141294ae0d810f5091c8bce139c349ae4781324
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-10-28 01:24:46 +0900

    Refactor MicropostController to include createdAt and updatedAt fields in the index response

--- backend/src/features/micropost/micropost.controller.ts
+++ backend/src/features/micropost/micropost.controller.ts
@@ -1,6 +1,7 @@
 import { Controller, Get, Post, Body, Param, NotFoundException } from '@nestjs/common';
 import { MicropostService } from './micropost.service';
 import { Micropost } from '@prisma/client';
+import { DetailedMicropost } from '@/shared/types/micropost.types';
 
 @Controller('microposts')
 export class MicropostController {
@@ -18,18 +19,13 @@ export class MicropostController {
 
     // マイクロポストに紐づく、「紐づいたユーザーID、ユーザー名」「いいねの数」も取得
   @Get()
-  async index(): Promise<(Omit<Micropost, 'createdAt' | 'updatedAt'> & {
-    user: { id: number; name: string };
-    likesCount: number;
-    createdAt: string;
-    updatedAt: string;
-  })[]> {
+  async index(): Promise<DetailedMicropost[]> {
     return this.micropostService.all();
   }
 
   // マイクロポストに紐づく、「コメント 紐づいたユーザーID、ユーザー名、アバター」「いいねの数」も取得
   @Get(':id')
-  async findOne(@Param('id') id: string) {
+  async findOne(@Param('id') id: string): Promise<DetailedMicropost> {
     const micropost = await this.micropostService.findOne(+id);
     if (!micropost) {
       throw new NotFoundException(`Micropost with ID ${id} not found`);
