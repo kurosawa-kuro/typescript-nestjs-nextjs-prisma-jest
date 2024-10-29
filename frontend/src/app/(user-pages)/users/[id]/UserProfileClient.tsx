bc9d0aeb4826a6b105f1fe1b063717fd5e536267
Toshifumi Kurosawa <kuromailserver@gmail.com>
2024-10-29 22:33:57 +0900

    Refactor import paths for user types in frontend tests

--- frontend/src/app/(user-pages)/users/[id]/UserProfileClient.tsx
+++ frontend/src/app/(user-pages)/users/[id]/UserProfileClient.tsx
@@ -4,7 +4,7 @@ import { useEffect, useState } from 'react';
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
 import { UserDetails } from '@/types/user';
-import { useUserProfileStore } from '@/store/userProfileStore';
+import { useUserProfileStore } from '@/store/UserProfileStore';
 import LoadingSpinner from '@/components/LoadingSpinner';
 import Link from 'next/link';
 
--- frontend/src/app/(user-pages)/users/[id]/page.tsx
+++ frontend/src/app/(user-pages)/users/[id]/page.tsx
@@ -3,14 +3,7 @@ import UserProfileClient from './UserProfileClient';
 import { headers } from 'next/headers';
 
 export default async function UserProfilePage({ params }: { params: { id: string } }): Promise<JSX.Element> {
-  // ログインユーザーのIDを取得ではなく、指定されたユーザーのIDを取得
   const paramsId = parseInt(params.id);
-  console.log('paramsId', paramsId);
-  // const userId = getUserIdFromHeaders();
-  
-  // if (!userId) {
-  //   return <div>User ID not found</div>;
-  // }
 
   try {
     const userDetails = await getUserDetails(paramsId);
@@ -23,22 +16,5 @@ export default async function UserProfilePage({ params }: { params: { id: string
   } catch (error) {
     console.error('Error fetching user details:', error);
     return <div>Error loading user details</div>;
-  }
-}
-
-function getUserIdFromHeaders(): number | null {
-  const headersList = headers();
-  const userDataString = headersList.get('x-user-data');
-  
-  if (!userDataString) {
-    return null;
-  }
-
-  try {
-    const userData = JSON.parse(userDataString);
-    return typeof userData.id === 'number' ? userData.id : null;
-  } catch (error) {
-    console.error('Error parsing user data:', error);
-    return null;
   }
 }
